2eaf5e2254e64a7485aeacc3bdd1bddf77e757ce
Felix Hassemer <felixhassemer@gmail.com>
2017-01-31 21:52:39 +0100

    recommit with working version

--- autodrawing4/sketch.js
+++ autodrawing4/sketch.js
@@ -3,18 +3,34 @@
 // GLOBAL variables
 
 var smoothCorner = 5;
+var colorToggle = false;
+
+// WINDOW
+var w = {
+  width: 1920,
+  height: 1200
+}
+
+// black and white
+var bw = {
+  bgnd: 0,
+  f: 100,
+  s: 0
+}
 
 var col = {
+  hueMod: 0,
+  satMod: 100,
   bgnd: 0,
-  f: 255,
+  f: 0,
   s: 0
 }
 
 var fr = 60;
 
-// s is sound
+// s is SOUND
 var s = {
-  bpm: 120,
+  bpm: 90,
   song: null,
   amp: null,
   vol: null,
@@ -28,38 +44,44 @@ var s = {
 // ----------------------------------------------------------
 
 function preload() {
-  s.song = loadSound("data/ificouldfeelagain.mp3");
+  s.song = loadSound("data/bouncin.mp3");
 }
 
 function setup() {
-  createCanvas(800, 800);
-  background(col.bgnd);
+  w.width = windowWidth;
+  w.height = windowHeight;
+  createCanvas(w.width, w.height);
   frameRate(fr);
+  // devicePixelScaling(false);
+  // var display = displayDensity();
+  // pixelDensity(display);
+
+  colorMode(HSB);
+
+  background(bw.bgnd);
 
   s.song.play();
-  s.amp = new p5.Amplitude(0.1);
+  s.amp = new p5.Amplitude(0.01);
   s.amp.toggleNormalize(1);
 
-  stroke(col.s);
-  strokeWeight(1);
+  strokeWeight(2);
   rectMode(CENTER);
-  fill(col.f);
 }
 
 function draw() {
   translate(canvas.width/2, canvas.height/2);
 
+  if (colorToggle) {
+    cycleColor();
+  } else {
+    blackWhite();
+  }
+
   // map audio level to size - setSize(max)
   obj.setSize(300);
 
   // calculate Target point - setTarget(border)
-  obj.setTarget(80);
-
-  if (frameCount % 2 == 0) {
-    fill(col.f);
-  } else {
-    fill(0);
-  }
+  obj.setTarget(150);
 
   // easing
   obj.ease(0.1);
@@ -95,9 +117,18 @@ var obj = {
     if ((mirror === undefined) || (mirror != -1)) {
       mirror = 1;
     }
-    if (s.vol < 0.2) {
-      ellipse(mirror*obj.xPos, mirror*obj.yPos, 5, 5);
-    } else if (s.vol < 0.7) {
+    if (s.vol < 0.4) {
+      if (colorToggle) {
+        col.f = color(col.hueMod, 100, 100);
+        fill(col.f);
+        noStroke();
+      } else {
+        bw.f = 100;
+        fill(100);
+        stroke(0);
+      }
+      ellipse(mirror*obj.xPos, mirror*obj.yPos, 12, 12);
+    } else if (s.vol < 0.9) {
       ellipse(mirror*obj.xPos, mirror*obj.yPos, obj.size, obj.size);
     } else {
       rect(mirror*obj.xPos, mirror*obj.yPos, obj.size, obj.size, smoothCorner);
@@ -110,11 +141,53 @@ var obj = {
       obj.yTarget = random(-canvas.height/2+border, canvas.height/2-border);
     }
   }
+}
 
+function blackWhite() {
+  if (frameCount % 2 == 0) {
+    bw.f = 100;
+    bw.s = 0;
+    fill(bw.f);
+    stroke(bw.s);
+  } else {
+    bw.f = 0;
+    bw.s = 100;
+    fill(bw.f);
+    stroke(bw.s);
+  }
+}
+
+function cycleColor() {
+  col.satMod = map(s.amp.getLevel(), 0, 1, 0, 100);
+  col.f = color(col.hueMod, 0, 0);
+  col.s = color(col.hueMod, col.satMod, 100);
+  stroke(col.s);
+  fill(col.f);
+
+  if (frameCount % 3 == 1) {
+    col.hueMod += 1;
+    if (col.hueMod == 360) {
+      col.hueMod = 1;
+    }
+  }
+}
+
+function keyPressed() {
+  if (keyCode == 32) {
+    background(bw.bgnd);
+    colorToggle = !colorToggle;
+  }
 }
 
 function clearScreen(beats) {
   if (frameCount % (s.oneBeat()*beats) == 0) {
-    background(col.bgnd);
+    background(bw.bgnd);
   }
+}
+
+function windowResized() {
+  w.width = windowWidth;
+  w.height = windowHeight;
+  resizeCanvas(w.width, w.height);
+  background(bw.bgnd);
 }
